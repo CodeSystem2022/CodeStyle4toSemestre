3954fc53c513edae99fa72a860b9f84f9ad421a3
Dana Angellotti <94261151+Danangellotti@users.noreply.github.com>
2023-09-18 10:36:13 -0300

    Contador de productos en el botón del carrito
    e-commerce javascript

--- Tecnicatura4_js/e-commerce2022/client/js/cart.js
+++ Tecnicatura4_js/e-commerce2022/client/js/cart.js
@@ -2,6 +2,7 @@ const modalContainer = document.getElementById("modal-container");
 const modalOverlay = document.getElementById("modal-overlay");
 
 const cartBtn = document.getElementById("cart-btn");
+const cartCounter = document.getElementById("cart-counter");
 
 const displayCart = () => {
   modalContainer.innerHTML = "";
@@ -53,11 +54,13 @@ const displayCart = () => {
         product.quanty--;
         displayCart();
       }
+      displayCartCounter();
     });
     const increse = modalBody.querySelector(".quantity-btn-increse");
     increse.addEventListener("click", () => {
       product.quanty++;
       displayCart();
+      displayCartCounter();
     });
 
     //delete
@@ -85,4 +88,15 @@ const deleteCartProduct = (id) => {
   const foundId = cart.findIndex((element) => element.id === id);
   cart.splice(foundId, 1);
   displayCart();
+  displayCartCounter();
+};
+
+const displayCartCounter = () => {
+  const cartLength = cart.reduce((acc, el) => acc + el.quanty, 0);
+  if (cart.length > 0) {
+    cartCounter.style.display = "block";
+    cartCounter.innerText = cartLength;
+  } else {
+    cartCounter.style.display = "none";
+  }
 };

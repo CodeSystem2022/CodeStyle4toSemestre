e27763607d6c1e538faa04b865784d893fc3ccb5
Dana Angellotti <94261151+Danangellotti@users.noreply.github.com>
2023-10-10 08:14:23 -0300

    agrego prueba , manejo de errores y utilización de returning
    PERN

--- PERN-stack/src/controllers/tareas.controller.js
+++ PERN-stack/src/controllers/tareas.controller.js
@@ -1,22 +1,29 @@
-import { pool } from '../db.js';
+import { pool } from "../db.js";
 
-export const listarTareas = (req, res) => res.send('obteniendo tareas');
+export const listarTareas = (req, res) => res.send("obteniendo tareas");
 
-export const listarTarea = (req, res) =>res.send('obteniendo tarea unica');
+export const listarTarea = (req, res) => res.send("obteniendo tarea unica");
 
-export const crearTarea = async(req, res) => {
-    const { titulo, descripcion } = req.body;
-    
+export const crearTarea = async (req, res, next) => {
+  const { titulo, descripcion } = req.body;
 
-    try {   const {rows} = await pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2', [titulo,descripcion]);
-    console.log(rows);
-    res.send('creando tarea');
-    } catch (error;) {
-        console.log("algo salio mal");
+  try {
+    const result = await pool.query(
+      "INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2) RETURNING *",
+      [titulo, descripcion]
+    );
+    res.json(result.rows[0]);
+    console.log(result.rows[0]);
+  } catch (error) {
+    if (error.code === "23505") {
+      return res.send("ya existe una tarea con ese titulo");
     }
+    console.log(error);
+    next(error);
+  }
+};
 
-}
+export const actualizarTarea = (req, res) =>
+  res.send("actualizando tarea unica");
 
-export const actualizarTarea = (req, res) => res.send('actualizando tarea unica');
-
-export const eliminarTarea = (req, res) => res.send('eliminando tarea');
+export const eliminarTarea = (req, res) => res.send("eliminando tarea");

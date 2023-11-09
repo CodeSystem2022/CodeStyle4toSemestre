d19b5dd567365cc695d66035dd4edb6b4eab787f
Florencia Micaela Oviedo <flor.oviedo2010@gmail.com>
2023-11-08 22:03:32 -0300

    clase 12 Node

---
+++ Tecnicatura4_Node/02-Fundamentos_node/conceptos/globales.js
@@ -0,0 +1,18 @@
+// this === global = true
+
+//Mostrar algo en consola
+//console.log();
+
+//Mostrar un mensaje en forma de error
+//onsole.error();
+
+//Ejecutar un codigo despues de un intervalo de tiempo
+//setTimeout(() => {})
+
+//Ejecutar un codigo cada intervalo de tiempo
+//setInterval(() =>{})
+
+//Da prioridad de ejecucion a una funcion asincronica
+//setImmediate(() =>{})
+
+console.log(setInterval);
